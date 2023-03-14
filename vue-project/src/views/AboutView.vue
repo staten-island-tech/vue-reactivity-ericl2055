5f03939561f0c4efa7e512006a6f10b229273ad2
ericl2055 <eric_lin2055@icloud.com>
2023-03-14 14:00:34 -0400

    stuff

--- vue-project/src/views/AboutView.vue
+++ vue-project/src/views/AboutView.vue
@@ -1,8 +1,32 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <button @click="authS" v-if="loggedIn">logged in</button>
+    <button v-else>logged out</button>
+    <ul>
+      <li v-for="card in cards" :key="card">{{ card }}</li>
+    </ul>
   </div>
 </template>
+<script>
+export default {
+  name: 'about',
+  components: {},
+  data() {
+    return {
+      loggedIn: true
+    }
+  },
+  methods: {
+    authState: function () {
+      if (this.loggedIn === false) {
+        this.loggedIn === true
+      } else {
+        this.loggedIn === false
+      }
+    }
+  }
+}
+</script>
 
 <style>
 @media (min-width: 1024px) {
