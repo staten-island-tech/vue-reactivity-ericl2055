d9ecad498f38b8b4d344796253cff01384b5af98
ericl2055 <eric_lin2055@icloud.com>
2023-03-24 10:21:14 -0400

    Update HomeView.vue

--- vue-project/src/views/HomeView.vue
+++ vue-project/src/views/HomeView.vue
@@ -3,7 +3,7 @@
     <ResetButton @click="build.length = 0">Reset Build</ResetButton>
     <div class="about">
       <Card
-        @click="build.cpu.push(Card.title, Card.price)"
+        @click="build.push(Card.title, Card.price)"
         v-for="Card in Card"
         :key="Card.title"
         :title="Card.title"
@@ -27,122 +27,7 @@ export default {
   },
   data() {
     return {
-      Card: [
-        {
-          title: 'Ryzen 5 5600X',
-          image: '',
-          price: '$154.00',
-          description: ''
-        },
-        {
-          title: 'Ryzen 5 5600',
-          image: '',
-          price: '$139.79',
-          description: ''
-        },
-        {
-          title: 'Ryzen 5 7600X',
-          image: '',
-          price: '$244.44',
-          description: ''
-        },
-        {
-          title: 'Ryzen 5 7600',
-          image: '',
-          price: '$234.00',
-          description: ''
-        },
-        {
-          title: 'Ryzen 7 5700X',
-          image: '',
-          price: '$185.99',
-          description: ''
-        },
-        {
-          title: 'Ryzen 7 7700X',
-          image: '',
-          price: '$338.00',
-          description: ''
-        },
-        {
-          title: 'Ryzen 7 7700',
-          image: '',
-          price: '$154.00',
-          description: ''
-        },
-        {
-          title: 'Ryzen 7 5800X',
-          image: '',
-          price: '$209.00',
-          description: ''
-        },
-        {
-          title: 'Ryzen 9 5900X',
-          image: '',
-          price: '$340.00',
-          description: ''
-        },
-        {
-          title: 'Ryzen 9 5950X',
-          image: '',
-          price: '$437.85',
-          description: ''
-        },
-        {
-          title: 'Ryzen 9 7900X',
-          image: '',
-          price: '$560.00',
-          description: ''
-        },
-        {
-          title: 'Ryzen 9 7950X',
-          image: '',
-          price: '$432.00',
-          description: ''
-        },
-        {
-          title: 'i9 13900',
-          image: '',
-          price: '$519.99',
-          description: ''
-        },
-        {
-          title: 'i9 13900K',
-          image: '',
-          price: '$569.99',
-          description: ''
-        },
-        {
-          title: 'i9 12900',
-          image: '',
-          price: '$539.99',
-          description: ''
-        },
-        {
-          title: 'i7 13700',
-          image: '',
-          price: '$319.99',
-          description: ''
-        },
-        {
-          title: 'i7 13700K',
-          image: '',
-          price: '$417.99',
-          description: ''
-        },
-        {
-          title: 'i7 12700',
-          image: '',
-          price: '$289.99',
-          description: ''
-        },
-        {
-          title: 'i7 12700K',
-          image: '',
-          price: '$319.99',
-          description: ''
-        }
-      ],
+      Card: [],
       build: [
         {
           casefan: '',
