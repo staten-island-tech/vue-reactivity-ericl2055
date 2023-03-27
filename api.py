adca1113d02e9a47cb3717c999e9544ba4aca498
BSelyomin <bogdan.selyomin@gmail.com>
2023-03-27 14:01:28 -0400

    3/27

--- api.py
+++ api.py
@@ -1,10 +1,12 @@
 from pcpartpicker import API
+import json
 
 api = API()
 all_data = api.retrieve_all()
 
-n = all_data.to_json()
-
-f = open("vue-project/src/assets/data.json", "w")
-f.write(n)
-f.close()
+n = json.loads(all_data.to_json())
+for x in n:
+    print(x)
+    f = open("vue-project/src/data/" + str(x) + ".json", "x")
+    f.write("{ 'data': " + str(n[x]) + "}")
+    f.close()
