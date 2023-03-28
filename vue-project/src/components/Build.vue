89780db8418037e5e3a748a60921c9143c70f22a
ericl2055 <eric_lin2055@icloud.com>
2023-03-28 13:58:57 -0400

    Create Build.vue

---
+++ vue-project/src/components/Build.vue
@@ -0,0 +1,20 @@
+<template>
+  <div></div>
+</template>
+
+<script>
+export default {
+  name: 'Build'
+  props: [{items}]
+}
+</script>
+
+<style>
+div {
+  display: flex;
+  flex-wrap: wrap;
+}
+img {
+  width: 225px;
+}
+</style>
